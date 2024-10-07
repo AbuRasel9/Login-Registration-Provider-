069096c2f3877341c5a8fb4a736267309bebfbaf
PTSL-RASEL <aburaselahamed99@gmail.com>
2024-10-07 12:37:41 +0600

    dipendency injection use for share preference and network call

--- lib/repo/api/api.dart
+++ lib/repo/api/api.dart
@@ -1,13 +1,46 @@
 import 'dart:convert';
 
 import 'package:http/http.dart';
+import 'package:untitled6/repo/sharePrefs/share_prefs.dart';
 
-Future<dynamic> postRequest(body, {required String url}) async {
-  Response response = await post(Uri.parse(url),    headers: {
-    "Content-Type": "application/json",
-    "Accept": "application/json",
-  },
-    body: jsonDecode(body)
+import '../../utils/service/di_service.dart';
 
-  );
+class Api {
+  final diSharePrefs=di.get<SharePrefs>();
+
+
+  //post method
+
+  Future<dynamic> postRequest(body, {required String url}) async {
+    try{
+      Response response = await post(Uri.parse(url),    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        "token":diSharePrefs.token ?? ""
+      },
+          body: jsonEncode(body,),
+
+      );
+      return dataStatusCheck(response);
+    }catch(e){
+      throw Exception("Api Call Error ${e}");
+
+
+    }
+
+
+  }
+
+
+  //get method
+
+  dynamic dataStatusCheck(Response response){
+
+    if(response.statusCode==200){
+      return response.body;
+    }else{
+      throw Exception("Api Call Error");
+    }
+  }
+
 }
